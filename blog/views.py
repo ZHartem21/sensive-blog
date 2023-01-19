b068fea9126d7904e17db6394faa19b7dc7c5b3d
ZHartem21 <zhartem21@gmail.com>
2023-01-20 00:03:57 +0300

    Cleanup the code

--- blog/views.py
+++ blog/views.py
@@ -6,28 +6,6 @@ POPULAR_POSTS_AMOUNT = 5
 POPULAR_TAGS_AMOUNT = 5
 
 
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
-    }
-
-
-
 def serialize_tag_optimised(tag):
     return {
         'title': tag.title,
@@ -65,8 +43,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    post = Post.objects.annotate(num_likes=Count('likes')).prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(num_posts=Count('posts')))).get(slug=slug)
+    comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -75,20 +53,16 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
-
-    related_tags = post.tags.all().annotate(num_posts=Count('posts'))
-
     serialized_post = {
         'title': post.title,
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.num_likes,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_optimised(tag) for tag in related_tags],
+        'tags': [serialize_tag_optimised(tag) for tag in post.tags.all()],
     }
 
     most_popular_tags = Tag.objects.popular()[:POPULAR_TAGS_AMOUNT]
